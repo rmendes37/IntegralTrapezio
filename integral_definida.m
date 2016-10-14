22a036ee12d214a0397db2853eecbfaa71727bd1
HP <HP@ARM-HP-educa>
2016-10-13 21:26:08 -0300

    Criando a função integral_definida e criando o Teste que verifica a existencia da função integral_definida

---
+++ integral_definida.m
@@ -0,0 +1,7 @@
+function [ output_args ] = integral_definida( input_args )
+%INTEGRAL_DEFINIDA Summary of this function goes here
+%   Detailed explanation goes here
+
+
+end
+
---
+++ integral_definidaTest.m
@@ -0,0 +1,7 @@
+function [ output_args ] = integral_definidaTest( input_args )
+%INTEGRAL_DEFINIDATEST Summary of this function goes here
+%   Detailed explanation goes here
+
+
+end
+
